65c0b38e14d5b3b43ed932afeb2d99001e7bc778
flakster <carlos.santamaria@gmail.com>
2020-02-14 16:24:44 -0500

    add the my_select method to the Enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -3,19 +3,34 @@ module Enumerable
   #definition of the my_each method
   def my_each
     return (to_enum(:my_each)) unless block_given? #no block given?, then return an enumerator
-    0.upto(self.length-1) do |i|
-      yield(self[i])
+    for element in self do
+      yield(element)
     end
+    self
   end
-  
+
   #definition of the my_each_with_index method
   def my_each_with_index
     return (to_enum(:my_each)) unless block_given? #no block given?, then return an enumerator
-    0.upto(self.length-1) do |i|
-      yield(self[i], i)
+    index=0
+    for element in self do
+      yield(element,index)
+      index+=1
     end
+    self
+  end
+
+  #definition of the my_select method
+  def my_select
+    return (to_enum(:my_each)) unless block_given? #no block given?, then return an enumerator
+    new_array=[]
+    self.my_each do |element|
+      new_array << element if yield(element)
+    end
+    new_array
   end
 end
 
+dogs= ["Gordis", "Chiquis", "Goro", "Duquesa", "Fenriz", "Ital", "Lemmy", "Pinpin"]
 
 
