944aababd5ec3d9229e0976f144abeb80263d17a
flakster <carlos.santamaria@gmail.com>
2020-02-14 14:16:08 -0500

    add the my_each_with_index method to the Enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -2,13 +2,20 @@ module Enumerable
 
   #definition of the my_each method
   def my_each
-    return (to_enum(:my_each)) unless block_given? #no block passed, then return an enumerator
+    return (to_enum(:my_each)) unless block_given? #no block given?, then return an enumerator
     0.upto(self.length-1) do |i|
       yield(self[i])
     end
   end
-
-
+  
+  #definition of the my_each_with_index method
+  def my_each_with_index
+    return (to_enum(:my_each)) unless block_given? #no block given?, then return an enumerator
+    0.upto(self.length-1) do |i|
+      yield(self[i], i)
+    end
+  end
 end
+
 
 
